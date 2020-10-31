dfbb29809616cfdd1847fccd718aeb3c662d475f
GabrielCabral352 <gabrielcabral352@gmail.com>
2020-10-31 09:17:11 -0300

    first try

--- webScraping.py
+++ webScraping.py
@@ -1,1 +1,18 @@
-print("Olá, Mundo")
+import time
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+url = "https://www.nba.com/stats/leaders/?Season=2019-20&SeasonType=Regular%20Season"
+
+option = Options()
+option.headless = True
+driver = webdriver.chrome()
+driver.get(url)
+driver.quit()   
+# pip install requests2
+# pip isntall pandas
+# pip install lxml
+# pip install beatifulsoup4
